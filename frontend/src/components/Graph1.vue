a282fd84ead3a9d31841daeb6f6a60c9862aaf75
Jiaxin Yang <yyjx0503@gmail.com>
2023-05-15 01:12:07 -0400

    Update Graph1.vue

--- frontend/src/components/Graph1.vue
+++ frontend/src/components/Graph1.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h3>Vue.js and D3 Line Chart</h3>
+      <h3>Top 10 Same Keywords</h3>
       <svg></svg>
     </div>
 </template>
@@ -14,79 +14,67 @@
     mounted() {
       const width = 500;
       const height = 350;
-      const data = [{ date: "24-Apr-07", amount: 93.24 },
-    { date: "25-Apr-07", amount: 95.35 },
-    { date: "26-Apr-07", amount: 98.84 },
-    { date: "27-Apr-07", amount: 99.92 },
-    { date: "30-Apr-07", amount: 99.8 },
-    { date: "1-May-07", amount: 99.47 },
-    { date: "2-May-07", amount: 100.39 },
-    { date: "3-May-07", amount: 100.4 },
-    { date: "4-May-07", amount: 100.81 },
-    { date: "7-May-07", amount: 103.92 },
-    { date: "8-May-07", amount: 105.06 },
-    { date: "9-May-07", amount: 106.88 },
-    { date: "10-May-07", amount: 107.34 },];
+      const data = this.$store.state.files.map((file) => ({name: file.name}))
+
+      const counts = data.reduce((acc, curr) => {
+        acc[curr.name] = (acc[curr.name] || 0) + 1;
+        return acc;
+        }, {});
+        const dataC = Object.entries(counts)
+          .map(([name, count]) => ({ name, count }))
+          .sort((a, b) => b.count - a.count) // sort by count in descending order
+          .slice(0, 10);
+
+      console.log(dataC);
+      
+    //   [{ Country: "United States", Value: 12394 },
+    // { Country: "Russia", Value: 6535 },
+    // { Country: "Germany", Value: 1684 },
+    // { Country: "France", Value: 2192 },
+    // { Country: "United Kingdom", Value: 1218 },
+    // { Country: "China", Value: 1147 },
+    // { Country: "Spain", Value: 839 },
+    // { Country: "Netherlands", Value: 1167 },
+    // { Country: "Italy", Value: 660 },
+    // { Country: "Israel", Value: 1032 }];
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 50 };
+    const svg = d3.select("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
+    .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // const svg = d3.select("svg").attr("width", width).attr("height", height);
+      
+      // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ 0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Y axis
+  var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(dataC.map(function(d) { return d.name; }))
+    .padding(.1);
+  svg.append("g")
+    .call(d3.axisLeft(y))
   
-      const svg = d3.select("svg").attr("width", width).attr("height", height);
-      const g = svg.append("g");
-  
-      //2. Parse the dates
-      const parseTime = d3.timeParse("%d-%b-%y");
-  
-      //3. Creating the Chart Axes
-      const x = d3
-        .scaleTime()
-        .domain(
-          d3.extent(data, function (d) {
-            return parseTime(d.date);
-          })
-        )
-        .rangeRound([0, width]);
-  
-      const y = d3
-        .scaleLinear()
-        .domain(
-          d3.extent(data, function (d) {
-            return d.amount;
-          })
-        )
-        .rangeRound([height, 0]);
-  
-      //4. Creating a Line
-      const line = d3
-        .line()
-        .x(function (d) {
-          return x(parseTime(d.date));
-        })
-        .y(function (d) {
-          return y(d.amount);
-        });
-  
-      //5. Appending the Axes to the Chart
-      g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(30,100)");
-  
-      g.append("g")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Number");    
-  
-      //6. Appending a path to the Chart
-      g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
-    },
-  };
+     //Bars
+  svg.selectAll("myRect")
+    .data(dataC)
+    .enter()
+    .append("rect")
+    .attr("x", x(0) )
+    .attr("y", function(d) { return y(d.name); })
+    .attr("width", function(d) { return x(d.count); })
+    .attr("height", y.bandwidth() )
+    .attr("fill", "#69b3a2")
+
+  }
+}
+     
 </script>
